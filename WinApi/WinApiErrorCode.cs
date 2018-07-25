74930d0018ce42387dc5c36c448a3fad90d32fed
Victor Milovanov <lost@losttech.software>
2018-07-25 00:04:41 -0700

    initial import

---
+++ WinApi/WinApiErrorCode.cs
@@ -0,0 +1,9 @@
+﻿// ReSharper disable InconsistentNaming
+namespace LostTech.Stack.WindowManagement.WinApi {
+    public enum WinApiErrorCode
+    {
+        ERROR_ACCESS_DENIED = 5,
+
+        ERROR_INVALID_WINDOW_HANDLE = 1400,
+    }
+}
