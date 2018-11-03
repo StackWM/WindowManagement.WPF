591295e51cf54dbeac31692dd20a7ebc4973bb81
Victor Milovanov <lost@losttech.software>
2018-11-02 20:39:14 -0700

    added ERROR_INVALID_CURSOR_HANDLE

--- WinApi/HResult.cs
+++ WinApi/HResult.cs
@@ -2,6 +2,7 @@
 namespace LostTech.Stack.WindowManagement.WinApi {
     public enum HResult:uint {
         TYPE_E_ELEMENTNOTFOUND = 0x8002802B,
+        ERROR_INVALID_CURSOR_HANDLE = 0x8007057A,
     }
 
     public static class HResultExtensions {
