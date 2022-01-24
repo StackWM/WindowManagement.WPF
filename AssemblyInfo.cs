772ae74986171554350c3b13f7aac9657eee119e
Victor Nova <lost@losttech.software>
2022-01-23 17:22:14 -0800

    updated copyright year

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.1.*")]
 [assembly: AssemblyCompany("Lost Tech LLC")]
 [assembly: AssemblyProduct("Stack")]
-[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2019")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2022")]
 
 [assembly: ComVisible(false)]
